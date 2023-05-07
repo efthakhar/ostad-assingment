3164389427b255cbc86a7d2a512871e092c9df79
Efthakhar <efthakharbin@gmail.com>
2023-05-08 01:50:52 +0600

    A12 Updated

--- A12/script.js
+++ A12/script.js
@@ -1,21 +1,24 @@
-// Get the input elements and the result element
+
 const weightInput = document.getElementById("weightInput");
 const heightInput = document.getElementById("heightInput");
 const calculateBtn = document.getElementById("calculateBtn");
 const resultElement = document.getElementById("result");
 
-// Add event listener to the calculate button
+
 calculateBtn.addEventListener("click", calculateBMI);
 
-// Calculate BMI and display the result
+
+
 function calculateBMI() {
     const weight = Number(weightInput.value);
     const height = Number(heightInput.value);
 
-    if (isNaN(weight) || isNaN(height)) {
-        resultElement.innerText = "Please enter valid numbers.";
+    if (isNaN(weight) || isNaN(height) || !weight || !height) {
+        resultElement.classList.remove('success')
+        resultElement.innerText = "Please enter valid numbers.";    
     } else {
         const bmi = weight / (height * height);
-        resultElement.innerText = `Your BMI is ${bmi.toFixed(2)}.`;
+        resultElement.innerText = `Your BMI is  ${bmi.toFixed(2)}.`;
+        resultElement.classList.add('success')
     }
 }
